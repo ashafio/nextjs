5022eeefaded7b83e66df6ee7dbe0b27be8b05a1
Abid Rahman Shafi <abidurrahman780@gmail.com>
2024-03-05 17:02:14 +0600

    NextJS

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function About(){
+    return <h1 className="text-center text-teal-500 font-bold text-2xl">About Me</h1>;
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,3 @@
+export default function About(){
+    return <h1 className="text-center text-teal-500 font-bold text-2xl">My Profile</h1>;
+}
